5d386b243ac36b00392c482fde46fb26e4a63f79
sefakor20 <sefakorhom2012@gmail.com>
2020-12-12 22:30:47 +0000

    API key removed

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <title>Access Google Maps API in PHP</title>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
     <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyChzF3o3zyM6XB8pCAksyv7bUYpCcJw_uk&callback=loadMap"
+      src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE&callback=loadMap"
       defer
     ></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
